2c9c51d232d8a381f0932f2aa58f85cde3f46847
H. M. Mehedi Hasan <141162383+Mehedi26696@users.noreply.github.com>
2025-04-21 00:40:54 +0600

    up

--- app.py
+++ app.py
@@ -59,6 +59,6 @@ if "mcq_data" in st.session_state:
         st.subheader("✅ Correct Answers")
         for idx, q in enumerate(st.session_state.mcq_data):
             correct_option = q["answer"]
-            correct_text = q["options"].get(correct_option, "[Invalid Answer]")
+            correct_text = q["options"][correct_option]
             st.markdown(f"**Q{idx+1}:** {q['question']}")
             st.markdown(f"✔️ Correct Answer: `{correct_option}) {correct_text}`")
